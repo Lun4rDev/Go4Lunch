fbfa21ade82c594b729ae081ca1a6f5a940c7be7
Mickaël Hernandez <mickael.hernandez9@gmail.com>
2018-02-01 02:53:12 +0100

    Started implementing Places API (asking permission to locate position)

--- app/src/main/java/com/hernandez/mickael/go4lunch/activities/MainActivity.kt
+++ app/src/main/java/com/hernandez/mickael/go4lunch/activities/MainActivity.kt
@@ -2,23 +2,40 @@ package com.hernandez.mickael.go4lunch.activities
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationProvider
 import android.os.Bundle
+import android.support.annotation.NonNull
 import android.support.design.widget.BottomNavigationView
 import android.support.design.widget.NavigationView
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
 import com.bumptech.glide.Glide
+import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.common.api.Status
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.places.GeoDataClient
 import com.google.android.gms.location.places.Place
+import com.google.android.gms.location.places.PlaceDetectionClient
+import com.google.android.gms.location.places.Places
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment
 import com.google.android.gms.location.places.ui.PlaceSelectionListener
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.hernandez.mickael.go4lunch.R
@@ -32,6 +49,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     /** Sign-in intent code */
     val RC_SIGN_IN = 123
     val RC_LOGOUT = 456
+    val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
 
     /** Shared preferences */
     lateinit var mSharedPrefs : SharedPreferences
@@ -45,7 +63,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     /** Bottom navigation adapter */
     private lateinit var pagerAdapter : BottomBarAdapter
 
+    /** Map fragment */
+    private var mMapFragment = SupportMapFragment()
+
+    /** Google Map object */
     private lateinit var mMap : GoogleMap
+
+    private var mLocationPermissionGranted = false
 
     /** Bottom navigation click listener */
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -69,18 +93,42 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         false
     }
 
+    private lateinit var mGeoDataClient: GeoDataClient
+
+    private lateinit var mPlaceDetectionClient: PlaceDetectionClient
+
+    private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+
+    private lateinit var mLastKnownLocation: Location
+
+    private val DEFAULT_ZOOM = 15f
+
+    private lateinit var mDefaultLocation: LatLng
+
     /** On class creation */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mSharedPrefs = getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE)
 
+        mMapFragment.getMapAsync(this)
+
+        // Construct a GeoDataClient.
+        mGeoDataClient = Places.getGeoDataClient(this, null)
+
+        // Construct a PlaceDetectionClient.
+        mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null)
+
+        // Construct a FusedLocationProviderClient
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+
         // Bottom navigation view and adapter init
         navView = findViewById(R.id.nav_view)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         viewPager.setPagingEnabled(false)
         pagerAdapter = BottomBarAdapter(supportFragmentManager)
-        pagerAdapter.addFragments(SupportMapFragment.newInstance())
+        pagerAdapter.addFragments(mMapFragment)
         pagerAdapter.addFragments(ListFragment())
         pagerAdapter.addFragments(PeopleFragment())
         viewPager.adapter = pagerAdapter
@@ -108,6 +156,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
         autocompleteFragment.setOnPlaceSelectedListener(listener)
 
+        // Fill UI with Firebase user data
         mUser = FirebaseAuth.getInstance().currentUser
         if(mUser != null){
             navView.getHeaderView(0).findViewById<TextView>(R.id.text_user_name).text = mUser!!.displayName
@@ -120,7 +169,82 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         mMap = p0!!
         mMap.uiSettings.isMyLocationButtonEnabled = true
         //gMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style))
+
+        // Turn on the My Location layer and the related control on the map.
+        updateLocationUI()
+
+        // Get the current location of the device and set the position of the map.
+        getDeviceLocation()
+
     }
+
+    private fun getLocationPermission() {
+        /*
+       * Request location permission, so that we can get the location of the
+       * device. The result of the permission request is handled by a callback,
+       * onRequestPermissionsResult.
+       */
+        if ((ContextCompat.checkSelfPermission(this.applicationContext,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) === PackageManager.PERMISSION_GRANTED))
+        {
+            mLocationPermissionGranted = true
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf<String>(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun updateLocationUI() {
+        try {
+            if (mLocationPermissionGranted) {
+                mMap.isMyLocationEnabled = true
+                mMap.uiSettings.isMyLocationButtonEnabled = true
+            } else {
+                mMap.isMyLocationEnabled = false
+                mMap.uiSettings.isMyLocationButtonEnabled = false
+                //mLastKnownLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message)
+        }
+
+    }
+
+    private fun getDeviceLocation() {
+        /*
+       * Get the best and most recent location of the device, which may be null in rare
+       * cases when a location is not available.
+       */
+        try
+        {
+            if (mLocationPermissionGranted)
+            {
+                val locationResult = mFusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        mLastKnownLocation = task.result
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(mLastKnownLocation.latitude,
+                                        mLastKnownLocation.longitude), DEFAULT_ZOOM))
+                    } else {
+                        //Log.d(TAG, "Current location is null. Using defaults.")
+                        //Log.e(TAG, "Exception: %s", task.getException())
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM))
+                        mMap.uiSettings.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        }
+        catch (e:SecurityException) {
+            Log.e("Exception: %s", e.message)
+        }
+    }
+
 
     /** Inflates the toolbar menu */
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
